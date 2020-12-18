bd82561dd26e7820e2e3d958aa1dd52f402c823a
isleilima <74622432+isleilima@users.noreply.github.com>
2020-12-18 00:06:48 -0300

    App-quiz

---
+++ App-quiz/js/funcoes.js
@@ -0,0 +1,27 @@
+function NumeroUm() {
+    var x = document.getElementById("numero-um");
+    x.disabled = true;
+    
+    alert("Resposta certa");
+}
+    
+function NumeroDois() {
+    var x = document.getElementById("numero-dois");
+    x.enable = false;
+
+    
+    alert("Resposta errada");
+
+ }
+ function NumeroTres() {
+    var x = document.getElementById("numero-tres");
+    x.disabled = false;
+
+    
+    alert("Resposta errada");
+
+ }
+    
+
+
+   
